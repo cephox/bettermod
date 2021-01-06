2f23ea3e49159964213a4e431b39b1e281910dc9
cephox <paul-stier@gmx.de>
2021-01-06 13:19:24 +0100

    Added database.py

---
+++ database.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+
+
+class Database:
+    def __init__(self, port: str = "27017"):
+        self.client = MongoClient("mongodb://localhost:" + port)
+        self.db = self.client["bettermod"]
+        self.settings = self.db["settings"]
+        self.users = self.db["users"]
+        self.guilds = self.db["guilds"]
+
+    def get_token(self):
+        token = self.settings.find_one()
+        return token["token"]
+
+    def set_token(self, token: str):
+        self.settings.remove(0)
+        self.settings.insert_one({"_id": 0, "token": token})
+
+
+database: Database = Database()
