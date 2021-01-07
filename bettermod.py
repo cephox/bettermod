bf42d0b3960776b1904532f3fea8ae2bfcc14047
cephox <paul-stier@gmx.de>
2021-01-07 13:58:55 +0100

    Added first cogs

--- bettermod.py
+++ bettermod.py
@@ -1,11 +1,15 @@
 #!/bin/python
 from database import database
-from util import check_for_token
+from util import check_for_token, add_cogs
 
 from discord.ext.commands import AutoShardedBot
+
+from cogs.moderation import Moderation
+from cogs.debug import Debugging
 
 check_for_token()
 
 bot: AutoShardedBot = AutoShardedBot("--")
 
-bot.start(database.get_token())
+add_cogs(bot, Debugging, Moderation)
+bot.run(database.get_token())
