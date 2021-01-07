806f8591cb510d3e8888bd2595c773e987a7573f
cephox <paul-stier@gmx.de>
2021-01-07 13:52:10 +0100

    Updated database implementation

--- database.py
+++ database.py
@@ -12,15 +12,21 @@ class Database:
     def reset(self):
         self.client.drop_database("bettermod")
 
-    def get_token(self):
-        token = self.settings.find_one()
-        if token is None:
+    def set_setting(self, name, value):
+        self.settings.delete_one({"name": name})
+        self.settings.insert_one({"name": name, "value": value})
+
+    def get_setting(self, name):
+        res = self.settings.find_one({"name": name})
+        if res is None:
             return None
-        return token["token"]
+        return res["value"]
+
+    def get_token(self):
+        return self.get_setting("token")
 
     def set_token(self, token: str):
-        self.settings.remove(0)
-        self.settings.insert_one({"_id": 0, "token": token})
+        self.set_setting("token", token)
 
 
 database: Database = Database()
