5499e289a0505a3dd377bbdbe8fdf070dce85c23
cephox <paul-stier@gmx.de>
2021-01-25 17:02:26 +0100

    Added global command error check

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,5 +1,4 @@
 from discord.ext.commands import Cog, Bot, command, Context
-from discord.ext.commands.errors import MissingRequiredArgument, MemberNotFound
 from discord.member import Member
 from discord.embeds import Embed
 from discord.colour import Color
@@ -15,7 +14,7 @@ class Moderation(Cog):
     async def info(self, ctx: Context, member: Member):
         """
         Displays information about a guild member
-        Syntax: <member>
+        <member>
         """
         embed: Embed = Embed(title=member.name,
                              color=member.color if member.color != Color(0x000) else Color(0x1bb6d1),
@@ -33,20 +32,6 @@ class Moderation(Cog):
                         value="✓" if member.guild_permissions.administrator else "✗")
         embed.add_field(name="joined", value=f"{member.joined_at}")
         embed.add_field(name="created", value=f"{member.created_at}")
-        await ctx.send(embed=embed)
-
-    @info.error
-    async def info_error(self, ctx: Context, error):
-        embed = Embed(color=Color(0xff0000))
-
-        if isinstance(error, MissingRequiredArgument):
-            embed.add_field(name="Syntax",
-                            value=f"`{await ctx.bot.get_prefix(ctx)}info <member (id|mention)>`")
-        elif isinstance(error, MemberNotFound):
-            embed.description = "Member `" + error.argument + "` not found"
-        else:
-            embed.add_field(name="Unknown error", value=error)
-
         await ctx.send(embed=embed)
 
 
