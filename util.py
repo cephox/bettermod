20d14e8a6af93f3b22e893f3892a43f5919cc880
cephox <paul-stier@gmx.de>
2021-01-26 08:10:37 +0100

    Added check if members can interact with others

--- util.py
+++ util.py
@@ -1,5 +1,20 @@
+from discord.member import Member
+
 from database import database
 from sys import argv
+
+
+def can_interact(member: Member, target: Member):
+    if member.guild != target.guild:
+        return False
+
+    if member.guild.owner == member:
+        return True
+
+    if target.guild.owner == target:
+        return False
+
+    return member.top_role.position > target.top_role.position
 
 
 def check_for_token():
