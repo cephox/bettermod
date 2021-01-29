192452a95913436eb15da836f55d16f5e7554e2f
cephox <paul-stier@gmx.de>
2021-01-29 21:51:31 +0100

    Added missing permission error message

--- cogs/errors.py
+++ cogs/errors.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from discord import Embed
-from discord.ext.commands import Cog, Context, CommandNotFound
+from discord.ext.commands import Cog, Context, CommandNotFound, MissingPermissions
 
 from colors import Colors
 from translation import get_user_language
@@ -19,7 +19,14 @@ class Errors(Cog):
         if isinstance(error, CommandNotFound):
             print(error)
             embed.add_field(name=lang.f_error_command_not_found("`" + str(error).split(" ")[1].replace("\"", "") + "`"),
-                            value="** **")
+                            value="** **", inline=False)
+        elif isinstance(error, MissingPermissions):
+            missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+            if len(missing) > 2:
+                fmt = '`{}` ' + lang.add + ' `{}`'.format(", ".join(missing[:-1]), missing[-1])
+            else:
+                fmt = ' and '.join(["`" + i + "`" for i in missing])
+            embed.add_field(name=lang.error_missing_permissions, value=fmt, inline=False)
         await ctx.send(embed=embed)
 
 
